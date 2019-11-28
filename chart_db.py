6896e4144791f29512d77dd2a22b455fe8bc91ec
nachico30 <zxc_dog01@yahoo.co.jp>
2019-11-28 18:09:12 +0900

    add api and test

--- chart_db.py
+++ chart_db.py
@@ -34,47 +34,28 @@ def chart_db_insert(dbfile, insert_value):
         return 1
 
 
-def numbers_trainingdata_select(dbfile, keta):
+def chart_db_select(dbfile):
 
     with closing(sqlite3.connect(dbfile)) as conn:
+        conn.row_factory = dict_factory
         c = conn.cursor()
-
-        # ランダムフォレストの学習用にリハ後と抽せん後の角度ペアを取得
-        if keta == '1':
-            select_sql = 'select N3_H_PRESET as t, N3_H_AFTERDEGREE as a '
-        elif keta == '2':
-            # select_sql = 'select N3_J_PRESET as t, N3_J_AFTERDEGREE as a '
-            select_sql = 'select N3_J_TESTSHOT as t, N3_J_AFTERDEGREE as a '
-        elif keta == '3':
-            select_sql = 'select N3_I_PRESET as t, N3_I_AFTERDEGREE as a '
-        elif keta == '4':
-            # select_sql = 'select N4_S_PRESET as t, N4_S_AFTERDEGREE as a '
-            select_sql = 'select N4_S_TESTSHOT as t, N4_S_AFTERDEGREE as a '
-        elif keta == '5':
-            # select_sql = 'select N4_H_PRESET as t, N4_H_AFTERDEGREE as a '
-            select_sql = 'select N4_H_TESTSHOT as t, N4_H_AFTERDEGREE as a '
-        elif keta == '6':
-            # select_sql = 'select N4_J_PRESET as t, N4_J_AFTERDEGREE as a '
-            select_sql = 'select N4_J_TESTSHOT as t, N4_J_AFTERDEGREE as a '
-        elif keta == '7':
-            # select_sql = 'select N4_I_PRESET as t, N4_I_AFTERDEGREE as a '
-            select_sql = 'select N4_I_TESTSHOT as t, N4_I_AFTERDEGREE as a '
-        elif keta == 'p1':
-            select_sql = 'select N3_H_TESTSHOT as t, N3_H_PRESET as a '
-        elif keta == 'p2':
-            select_sql = 'select N3_J_TESTSHOT as t, N3_J_PRESET as a '
-        elif keta == 'p3':
-            select_sql = 'select N3_I_TESTSHOT as t, N3_I_PRESET as a '
-        elif keta == 'p4':
-            select_sql = 'select N4_S_TESTSHOT as t, N4_S_PRESET as a '
-        elif keta == 'p5':
-            select_sql = 'select N4_H_TESTSHOT as t, N4_H_PRESET as a '
-        elif keta == 'p6':
-            select_sql = 'select N4_J_TESTSHOT as t, N4_J_PRESET as a '
-        elif keta == 'p7':
-            select_sql = 'select N4_I_TESTSHOT as t, N4_I_PRESET as a '
-        select_sql += 'from N_RESULT where KAI >= 5235 order by KAI asc'
+        # chart.dbの中身を素直にselect
+        select_sql = textwrap.dedent('''\
+            select
+            OPENTIME,CLOSETIME,OPEN,HIGH,LOW,CLOSE,VOLUME,BASEVOLUME,
+            OPEN_JPY,HIGH_JPY,LOW_JPY,CLOSE_JPY,
+            OPEN_USD,HIGH_USD,LOW_USD,CLOSE_USD from CHART
+            order by OPENTIME desc
+        ''')
         c.execute(select_sql)
         result = c.fetchall()
 
+        # dictを返却
         return result
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
